4518664a882c8b3183566be7c8074fa8f916a819
Harun Bülbül <95485758+HarunBulbull@users.noreply.github.com>
2024-11-06 13:40:01 +0300

    Keşfet ekranı

---
+++ front/src/components/user/User.jsx
@@ -0,0 +1,22 @@
+import './User.css'
+import React from 'react';
+import { Flex } from 'antd';
+
+function User({ info }) {
+
+  return (
+      <Flex gap="middle" align='center'>
+        <img src={info.picture} />
+        {info.class ?
+          <Flex vertical={true} >
+            <h3>{info.name}</h3>
+            <p>{info.class}</p>
+          </Flex> 
+          :
+          <h2>{info.name}</h2>
+        }
+      </Flex>
+  )
+}
+
+export default User
