e66385fbe82e9c7e488ce5cee4f16d3c0e5a08fa
Harun Bülbül <95485758+HarunBulbull@users.noreply.github.com>
2024-12-16 18:26:06 +0300

    kuveyt

--- back/routes/index.js
+++ back/routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const getTokenRoute = require("./getToken.js");
 const getOrganizationsRoute = require('./getOrganizations.js');
 const getInfoRoute = require('./getInfo.js');
-const paymentRoute = require('./payment.js');
-const paytenRoute = require('./payten.js');
+const kuveytRoute = require('./payment/kuveyt.js');
+const paytenRoute = require('./payment/payten.js');
 
 router.use("/token", getTokenRoute);
 router.use("/organization", getOrganizationsRoute);
 router.use("/info", getInfoRoute);
-router.use("/payment", paymentRoute);
+router.use("/kuveyt", kuveytRoute);
 router.use("/payten", paytenRoute);
 
 module.exports = router;
